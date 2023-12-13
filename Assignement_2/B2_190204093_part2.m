1e6bfcd3fae4a9c76b3f1eaca63db0a78eaa6bc2
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2023-12-14 02:42:22 +0600

    Add files via upload

---
+++ Assignement_2/B2_190204093_part2.m
@@ -0,0 +1,29 @@
+% a. Take an RGB input image and convert it into a grayscale image.
+rgbImage = imread('Cameraman.jpg');  % Replace with your actual RGB image filename
+
+% Check if the image is truecolor (3 channels)
+if size(rgbImage, 3) == 3
+    % Convert RGB to grayscale using a weighted sum of RGB channels
+    grayImage = rgb2gray(rgbImage);
+else
+    % Image is already grayscale
+    grayImage = rgbImage;
+end
+
+% b. Extract the dimension of the grayscale image (say R).
+[m,n] = size(grayImage);
+
+% c. For every column of bit depth of R, starting from the LSB, take the column number of the bit positions (say k) of R.
+bitDepth = 8;  % Assuming an 8-bit image
+for k = bitDepth:-1:1
+    % d. For every k, calculate X, where X is 2 to the power of that column number.
+    X = 2^(bitDepth - k);
+
+    % e. Calculate the bitwise AND operation for every pixel of the image using the bitand function.
+    S = bitand(grayImage, X);
+
+    % f. Show the output images (S) for every bit position.
+    subplot(2,5,k);
+    imshow(S, []);
+    title(['Bit Position: ' num2str(k)]);
+end
